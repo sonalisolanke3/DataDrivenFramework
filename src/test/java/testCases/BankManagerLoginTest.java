11132813e81ae2c0a034311ed9ede2ca15236f32
sonalisolanke3 <sonali.solanke3@gmail.com>
2022-07-26 12:06:16 +0530

    The First Commit

---
+++ src/test/java/testCases/BankManagerLoginTest.java
@@ -0,0 +1,20 @@
+package testCases;
+
+import org.apache.commons.codec.binary.Base32;
+import org.testng.annotations.Test;
+
+import baseTest.BaseTest;
+
+
+
+public class BankManagerLoginTest extends BaseTest{
+	
+	@Test
+	public void loginAsBankManager() {
+		
+		click("bmlBtn_XPATH");
+		isElementPresent("addCustomerBtn_XPATH");
+		
+	}
+
+}
